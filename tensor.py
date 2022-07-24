b4d87f681caa46f55cc5712ddd39a78a2a270981
Jim <104743175+eljimmo@users.noreply.github.com>
2022-07-24 12:48:29 -0500

    Create tensor.py

---
+++ tensor.py
@@ -0,0 +1,10 @@
+import tensorflow as tf 
+
+if __name__=='__main__':
+    
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+print("x_train.shape = ", x_train.shape)
+print("y_train.shape = ", y_train.shape)
+print("x_test.shape = ", x_test.shape)
+print("y_test.shape = ", y_test.shape)
